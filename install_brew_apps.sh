a783f0ab7a7db3474d3fbc129c4f895a7d21732e
Alec Brunelle <alec@alec.coffee>
2020-11-24 07:33:27 -0800

    Add steam, figma, drawio

--- install_brew_apps.sh
+++ install_brew_apps.sh
@@ -1,1 +1,2 @@
-brew install micro git gh hub terraform helm FairwindsOps/tap/pluto
+brew install FairwindsOps/tap/polaris
+brew install micro git gh hub kubectl minikube terraform helm FairwindsOps/tap/pluto heroku
--- install_brew_cask_apps.sh
+++ install_brew_cask_apps.sh
@@ -3,4 +3,5 @@ brew cask install aerial google-cloud-sdk kindle font-fira-code-nerd-font
     brave-browser spotify visual-studio-code discord spectacle notion
     authy firefox authy alfred nextdns zoom
     ishowu-instant insomnia dash bartender slack
-    docker microsoft-outlook obs loom
+    docker microsoft-outlook obs loom figma steam
+    drawio

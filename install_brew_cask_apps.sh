f1f78eeb57d7bf828433521b48647539ce9a5386
Alec Brunelle <alec@alec.coffee>
2022-11-02 14:48:30 -0400

    Update install_brew_cask_apps.sh

--- install_brew_cask_apps.sh
+++ install_brew_cask_apps.sh
@@ -1,11 +1,3 @@
  # Installs Brew Cask apps for a new Mac OS installation
 brew tap homebrew/cask-fonts
-brew install --cask aerial font-fira-code
-    brave-browser discord spectacle notion
-    authy raycast
-    insomnia dash bartender slack
-    docker steam drawio
-    postman kaleidoscope@2 amphetamine
-    1password battle-net deliveries proxyman medis numi
-    signal sonos tweetbot vlc twitter
-    1password/tap/1password-cli
+brew install --cask aerial font-fira-code brave-browser discord spectacle notion authy raycasti nsomnia dash bartender slack docker steam drawio postman kaleidoscope@2 amphetamine battle-net deliveries proxyman medis numi signal sonos tweetbot vlc twitter 1password/tap/1password-cli

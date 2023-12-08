e30db358a19217b66b0fe9d3300a5f9bc83329d7
Alec Brunelle <alec@alec.coffee>
2023-12-08 12:24:20 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,41 +1,30 @@
+eval "$(/Users/alec.brunelle/miniconda3/bin/conda shell.fish hook)"
+
+# Add brew to PATH
+set -x PATH /opt/homebrew/bin $PATH
+
+# init starship brew package fish integration
 starship init fish | source
 
-source ~/.iterm2_shell_integration.(basename $SHELL)
-    
-# google cloud sdk
-set -g -x "CLOUDSDK_PYTHON" "/usr/local/opt/python@3.9/bin/python3"
-set -g fish_user_paths "/usr/local/opt/python@3.9/bin" $fish_user_paths
-if [ -f '/Users/alecbrunelle/google-cloud-sdk/path.fish.inc' ]; . '/Users/alecbrunelle/google-cloud-sdk/path.fish.inc'; end
+# init kubectl alias
+alias k="kubectl"
 
 ## GIT ALIAS
 alias gitc="git commit -S -m"
 alias gitcp="git commit -m && git push --no-verify"
 
-## BAT ALIAS
-alias cat="bat"
-
-#DENO + ASDF
-set -x DENO_INSTALL ~/.asdf/shims/
-fish_add_path $DENO_INSTALL/bin
-
-# RASPBERRY PI
-# alias caspian_pi=""
-
-# UNITY NPM AUTH TOKEN
-set -x NPM_GITHUB_TOKEN $(op item get zixa6vzwpy6mcgokzuuhhh3pci --fields label=credential)
-
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
-# go tooling
-set -x GOPATH $HOME/go
-set -x PATH $GOPATH/bin:$PATH
-
-# gcp-infra tools
-
-# asdf
-source ~/.asdf/asdf.fish
-
-# 1password cli completions
+# init fish 1password cli completions
 op completion fish | source
+
+# init kubectl completions
+kubectl completion fish | source
+
+# init nice mac os settings
+# defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+set -x KUBE_EDITOR "code -w"
+

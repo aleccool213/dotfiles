ccb449d17dae9eb46ea1f88bf8bd4686b20e46f7
Alec Brunelle <alec@alec.coffee>
2023-12-09 16:12:14 -0500

    Update install_brew_cask_apps.sh

--- install_brew_cask_apps.sh
+++ install_brew_cask_apps.sh
@@ -1,3 +1,4 @@
  # Installs Brew Cask apps for a new Mac OS installation
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code discord spectacle notion authy raycast insomnia dash bartender slack docker steam postman proxyman medis numi signal vlc 1password/tap/1password-cli
+brew install --cask modern-csv spotify
